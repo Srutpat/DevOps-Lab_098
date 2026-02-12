c4199fedabe143652db44b83df5db5a1e6075c65
Shraddha Utpat <shraddha.utpat23@pccoepune.org>
2026-02-12 15:38:40 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class MainApp {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println("Sum = " + c.add(2, 3));
+        Utils.sayHi();
+    }
+}
